6d61de93884448912c55caf3f8360ac2d00e27ad
Justin Ryder <jlr2383@gmail.com>
2022-07-15 19:51:59 -0700

    made cards able to be moved wtf text why ui like this

--- GmtkGameJam2022/Assets/Scripts/Card.cs
+++ GmtkGameJam2022/Assets/Scripts/Card.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class Card : MonoBehaviour
 {
-    public TextMeshProUGUI TextMesh;
+    public TextMesh TextMesh;
     public SpriteRenderer Image;
-
+    public int LineLength = 40;
 
     public void LoadCard(string text, string imageName)
     {
-        TextMesh.text = text;
+        TextMesh.text = ResolveTextSize(text, LineLength);
         var image = Resources.Load<Sprite>(imageName);
         Image.sprite = image;
     }
@@ -27,5 +26,43 @@ public class Card : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private string ResolveTextSize(string input, int lineLength)
+    {
+
+        // Split string by char " "         
+        string[] words = input.Split(" "[0]);
+
+        // Prepare result
+        string result = "";
+
+        // Temp line string
+        string line = "";
+
+        // for each all words        
+        foreach(string s in words){
+            // Append current word into line
+            string temp = line + " " + s;
+
+            // If line length is bigger than lineLength
+            if(temp.Length > lineLength){
+
+                // Append current line into result
+                result += line + "\n";
+                // Remain word append into new line
+                line = s;
+            }
+            // Append current word into current line
+            else {
+                line = temp;
+            }
+        }
+
+        // Append last line into result        
+        result += line;
+
+        // Remove first " " char
+        return result.Substring(1,result.Length-1);
     }
 }
